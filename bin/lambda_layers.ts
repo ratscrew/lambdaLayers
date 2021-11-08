3476aaca54aef4ff961c03c0444f458c871f2923
Aidan Grace <atg@amazon.com>
2021-11-08 15:17:57 +0000

    updates

--- bin/lambda_layers.ts
+++ bin/lambda_layers.ts
@@ -2,16 +2,17 @@
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { LambdaLayersStack } from '../lib/lambda_layers-stack';
+import { MyPipelineStack } from '../lib/pipeline-stack'
 
 const app = new cdk.App();
-new LambdaLayersStack(app, 'LambdaLayersStack', {
+new MyPipelineStack(app, 'MyPipelineStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
 
   /* Uncomment the next line to specialize this stack for the AWS Account
    * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+   env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
---
+++ lib/pipeline-stack.ts
@@ -0,0 +1,51 @@
+import * as pipelines from '@aws-cdk/pipelines'
+import * as cdk from '@aws-cdk/core';
+import { LambdaLayersStack } from './lambda_layers-stack'
+
+
+export class MyPipelineStack extends cdk.Stack {
+    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+      super(scope, id, props);
+  
+      const pipeline = new pipelines.CodePipeline(this, 'Pipeline', {
+        synth: new pipelines.ShellStep('Synth', {
+          // Use a connection created using the AWS console to authenticate to GitHub
+          // Other sources are available.
+          input: pipelines.CodePipelineSource.connection('ratscrew/lambdaLayers', 'master', {
+            connectionArn: 'arn:aws:codestar-connections:eu-west-1:453380824957:connection/d20353d2-91a7-447a-905c-9c0c36de7dc6', // Created using the AWS console * });',
+          }),
+          commands: [
+            'npm ci',
+            'npm run build',
+            'npx cdk synth',
+          ],
+        }),
+      });
+  
+      // 'MyApplication' is defined below. Call `addStage` as many times as
+      // necessary with any account and region (may be different from the
+      // pipeline's).
+      pipeline.addStage(new MyApplication(this, 'Prod', { 
+        env:  { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+      }));
+    }
+  }
+  
+  /**
+   * Your application
+   *
+   * May consist of one or more Stacks (here, two)
+   *
+   * By declaring our DatabaseStack and our ComputeStack inside a Stage,
+   * we make sure they are deployed together, or not at all.
+   */
+  class MyApplication extends cdk.Stage {
+    constructor(scope: cdk.Construct, id: string, props?: cdk.StageProps) {
+      super(scope, id, props);
+  
+      const lambdaLayersStack = new LambdaLayersStack(this, 'lambdaLayers',{
+          env:  { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+      });
+
+    }
+  }
