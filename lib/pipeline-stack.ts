2dd1541397c4afb1a8f1195d47cc0b61ef8f0e2e
Aidan Grace <atg@amazon.com>
2021-11-11 14:34:10 +0000

    fix

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -3,12 +3,10 @@ import * as cdk from '@aws-cdk/core';
 import { LambdaLayersStack } from './lambda_layers-stack'
 
 
-interface myprops extends cdk.StackProps{
-  type:string
-}
+
 
 export class MyPipelineStack extends cdk.Stack {
-    constructor(scope: cdk.Construct, id: string, props?: myprops) {
+    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
       super(scope, id, props);
   
       const pipeline = new pipelines.CodePipeline(this, 'Pipeline', {
@@ -16,7 +14,7 @@ export class MyPipelineStack extends cdk.Stack {
         synth: new pipelines.ShellStep('Synth', {
           // Use a connection created using the AWS console to authenticate to GitHub
           // Other sources are available.
-          input: pipelines.CodePipelineSource.connection('ratscrew/lambdaLayers', props?.type == "prod"? 'master' : 'dev', {
+          input: pipelines.CodePipelineSource.connection('ratscrew/lambdaLayers', 'master', {
             connectionArn: 'arn:aws:codestar-connections:eu-west-1:453380824957:connection/d20353d2-91a7-447a-905c-9c0c36de7dc6', // Created using the AWS console * });',
           }),
           additionalInputs:{
@@ -39,7 +37,7 @@ export class MyPipelineStack extends cdk.Stack {
       // necessary with any account and region (may be different from the
       // pipeline's).
       pipeline.addStage(new MyApplication(this, 'Prod', { 
-        env:  { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION, db:"" },
+        env:  { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
       }));
 
     }
